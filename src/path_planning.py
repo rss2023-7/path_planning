ee5076ffe77d07a838690b3282255c01658ac2d3
ramgus <ramgus@mit.edu>
2023-04-18 14:56:22 -0400

    a-start still not working

--- src/path_planning.py
+++ src/path_planning.py
@@ -14,14 +14,22 @@ import heapdict
 
 
 
+
 class Node:
-    def __init__(self, cur, g_cost, h_cost, parent=None):
-        self.cur = cur
-        self.x, self.y = cur
+    def __init__(self, position, parent=None):
+        self.cur = position
+        self.x, self.y = position
         self.parent = parent
-        self.g_cost = g_cost
-        self.h_cost = h_cost
-        self.f_cost = g_cost + h_cost
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+    def __lt__(self, other):
+        return self.cur < other.cur
+
 
 
 class PathPlan(object):
@@ -111,24 +119,36 @@ class PathPlan(object):
 
     def get_cells(self, start, end):
         u = start
-        v = (end[0]-start[0], end[1]-start[1])
-        X, Y = (int(u[0]), int(u[1]))
+        v = (end[0] - start[0], end[1] - start[1])
+        X, Y = int(u[0]), int(u[1])
+
+        # Determine steps for X and Y
         stepX = 1 if v[0] > 0 else -1 if v[0] < 0 else 0
         stepY = 1 if v[1] > 0 else -1 if v[1] < 0 else 0
-        tMaxX = (np.round((u[0]*2+stepX)/2) - u[0]) / v[0]
-        tMaxY = (np.round((u[1]*2+stepY)/2) - u[1]) / v[1]
-        tDeltaX = abs(1/v[0])
-        tDeltaY = abs(1/v[1])
+
+        # Calculate tMax and tDelta values while avoiding division by zero
+        tMaxX = (np.round((u[0] * 2 + stepX) / 2) - u[0]) / v[0] if v[0] != 0 else np.inf
+        tMaxY = (np.round((u[1] * 2 + stepY) / 2) - u[1]) / v[1] if v[1] != 0 else np.inf
+        tDeltaX = abs(1 / v[0]) if v[0] != 0 else np.inf
+        tDeltaY = abs(1 / v[1]) if v[1] != 0 else np.inf
+
+        # Initialize cells list with starting point
         cells = [(X, Y)]
+
+        # Loop until tMaxX and tMaxY are both greater than or equal to 1
         while tMaxX < 1 or tMaxY < 1:
             if tMaxX < tMaxY:
                 tMaxX += tDeltaX
                 X += stepX
             else:
-                tMaxY += tDeltaY
+                tMaxY += tDeltaY if not np.isinf(tDeltaY) else 0
                 Y += stepY
+
+            # Add new cell to the list
             cells.append((X, Y))
+
         return cells
+
 
 
     def get_neighbors(self, node, map):
@@ -137,61 +157,65 @@ class PathPlan(object):
             for dy in [-1, 0, 1]:
                 if dx == 0 and dy == 0:
                     continue
-                new_x, new_y = int(node.x + dx), int(node.y + dy)
-                if 0 <= new_x < map.shape[0] and 0 <= new_y < map.shape[1] and map[new_x, new_y] == 0:
-                    neighbor = Node((new_x, new_y), node)
-                    neighbors.append(neighbor)
+                x, y = node.x + dx, node.y + dy
+                if 0 <= x < map.shape[0] and 0 <= y < map.shape[1] and map[int(x), int(y)] == 0 and self.path_clear(node.cur, (x, y), map):
+                    neighbors.append(Node((x, y), node))
         return neighbors
+
 
 
 
     def h_cost(self, node, goal):
         return abs(node.x - goal.x) + abs(node.y - goal.y)
 
-
     def plan_path(self, start_point, end_point, map):
         self.trajectory.clear()
 
         start_node = Node(self.convert_to_cell(start_point))
         end_node = Node(self.convert_to_cell(end_point))
+        #log start and end nodes x and y values
+        rospy.loginfo("Start node: " + str(start_node.x) + " " + str(start_node.y))
+        rospy.loginfo("End node: " + str(end_node.x) + " " + str(end_node.y))
+        open_set = heapdict.heapdict()
+        open_set[start_node] = 0
+        closed_set = set()
 
-        open_list = heapdict()
-        open_list[start_node] = 0
+        g_costs = {start_node: 0}
+        f_costs = {start_node: self.dist(start_node.cur, end_node.cur)}
 
-        closed_list = set()
-
-        g_cost = {start_node: 0}
-        f_cost = {start_node: self.dist(start_node.cur, end_node.cur)}
-
-        while open_list:
-            current_node, _ = open_list.popitem()
+        while open_set:
+            current_node, _ = open_set.popitem()
 
             if current_node == end_node:
                 path = []
                 while current_node is not None:
                     path.append(current_node)
                     current_node = current_node.parent
-                for node in path[::-1]:
+                path = path[::-1]
+                for node in path:
                     self.trajectory.addPoint(Node(self.convert_to_point(node.cur)))
                 break
 
-            closed_list.add(current_node)
+            closed_set.add(current_node)
 
             neighbors = self.get_neighbors(current_node, map)
             for neighbor in neighbors:
-                if neighbor in closed_list:
+                if neighbor in closed_set:
                     continue
 
-                tentative_g_cost = g_cost[current_node] + self.dist(current_node.cur, neighbor.cur)
-
-                if neighbor not in open_list or tentative_g_cost < g_cost[neighbor]:
+                tentative_g_cost = g_costs[current_node] + self.dist(current_node.cur, neighbor.cur)
+                if neighbor not in open_set or tentative_g_cost < g_costs[neighbor]:
                     neighbor.parent = current_node
-                    g_cost[neighbor] = tentative_g_cost
-                    f_cost[neighbor] = tentative_g_cost + self.dist(neighbor.cur, end_node.cur)
-                    open_list[neighbor] = f_cost[neighbor]
+                    g_costs[neighbor] = tentative_g_cost
+                    f_costs[neighbor] = g_costs[neighbor] + self.dist(neighbor.cur, end_node.cur)
+                    open_set[neighbor] = f_costs[neighbor]
+        #log the path
+        rospy.loginfo("Path: {}".format(self.trajectory))
+        rospy.loginfo("Path: {}".format(self.trajectory.toPoseArray()))
 
         self.traj_pub.publish(self.trajectory.toPoseArray())
         self.trajectory.publish_viz()
+
 
 
 
