070ad7f684f47c0b86310f88efcf834a529cc905
ramgus <ramgus@mit.edu>
2023-04-23 21:48:45 -0400

    need to fix occupancy grid correspondence

--- src/path_planning.py
+++ src/path_planning.py
@@ -5,10 +5,13 @@ import math
 import numpy as np
 from heapq import heappush, heappop
 from collections import deque
-from nav_msgs.msg import OccupancyGrid, Path, Odometry
+from nav_msgs.msg import OccupancyGrid, Path, Odometry, GridCells
 from geometry_msgs.msg import PoseStamped, Point, PoseArray
+from std_msgs.msg import Header, ColorRGBA
 import tf.transformations as trans
+from visualization_msgs.msg import Marker
 from utils import LineTrajectory
+from trajectory_builder import BuildTrajectory
 
 class PathPlan:
     def __init__(self):
@@ -24,7 +27,13 @@ class PathPlan:
         self.loaded_map = None
         self.origin_pub = rospy.Publisher("/map_origin", PoseStamped, queue_size=10)
         self.goal_pub = rospy.Publisher("/goal_point", PoseStamped, queue_size=10)
-        self.point_pub = rospy.Publisher("/point", PoseStamped, queue_size=10)
+        self.start_point_pub = rospy.Publisher("/start_point", PoseStamped, queue_size=10)
+        self.start_marker_pub = rospy.Publisher("/path_markers/start", Marker, queue_size=20)
+        self.end_marker_pub = rospy.Publisher("/path_markers/end", Marker, queue_size=20)
+        self.neighbor_grid_pub = rospy.Publisher("/neighbor_grid", GridCells, queue_size=10)
+        self.visited_nodes_pub = rospy.Publisher("/visited_grids", GridCells, queue_size=20)
+
+        self.counter = 0
 
     def get_pose(self, pose):
         x = pose.position.x
@@ -39,59 +48,78 @@ class PathPlan:
         self.loaded_map = map
 
 
-        
+
 
 
 
     def goal_cb(self, end):
         self.end = self.get_pose(end.pose)
-        rospy.loginfo("Goal received: {}".format(self.end))
+        self.counter += 1
         if self.map_received and hasattr(self, 'start'):
             self.plan_path(self.start, self.end, self.loaded_map)
 
     def odom_cb(self, odom):
         self.start = self.get_pose(odom.pose.pose)
-        rospy.loginfo("Start received: {}".format(self.start))
+        self.counter += 1
         if self.map_received and hasattr(self, 'end'):
             self.plan_path(self.start, self.end, self.loaded_map)
-
-
+ 
 
     def world_to_grid(self, x, y, map):
-        # min_world_x_coord = -(map.info.width * map.info.resolution - map.info.origin.position.x)
         max_world_x_coord = map.info.origin.position.x
         min_world_y_coord = -(map.info.height * map.info.resolution - map.info.origin.position.y)
-        # max_world_y_coord = map.info.origin.position.y
-        # min_grid_x_coord = 0
-        # max_grid_x_coord = map.info.width
-        # min_grid_y_coord = 0
-        # max_grid_y_coord = map.info.height 
         u = int(abs(y - min_world_y_coord) / map.info.resolution)
         v = int(abs(x - max_world_x_coord) / map.info.resolution)
         return u, v
+    
     def grid_to_world(self, u, v, map):
-        # min_world_x_coord = -(map.info.width * map.info.resolution - map.info.origin.position.x)
         max_world_x_coord = map.info.origin.position.x
         min_world_y_coord = -(map.info.height * map.info.resolution - map.info.origin.position.y)
-        # max_world_y_coord = map.info.origin.position.y
-        x = max_world_x_coord + v * map.info.resolution
-        y = min_world_y_coord + u * map.info.resolution
+        x = max_world_x_coord - (v * map.info.resolution)
+        y = (u * map.info.resolution) + (min_world_y_coord) 
         return x, y
+    
+    
 
     def plan_path(self, start_point, end_point, map):
-        # rospy.loginfo("Got to plan_path func, Map origin: {}".format(map.info.origin))
-        # rospy.loginfo("Start point: {}".format(start_point))
-        # rospy.loginfo("End point: {}".format(end_point))
-        # Publish point at grid value [10, 10]
-        # point = PoseStamped()
-        # point.header.frame_id = "map"
         start_u, start_v = self.world_to_grid(start_point[0], start_point[1], map)
         end_u, end_v = self.world_to_grid(end_point[0], end_point[1], map)
 
-        # rospy.loginfo("Start u: {}".format(start_u))
-        # rospy.loginfo("Start v: {}".format(start_v))
-        # rospy.loginfo("End u: {}".format(end_u))
-        # rospy.loginfo("End v: {}".format(end_v))
+        def visualize_marker(u, v, map, publisher):
+            point = self.grid_to_world(u, v, map)
+            marker = Marker()
+            header = Header()
+            header.frame_id = "map"
+            marker.header = header
+            marker.id = 0
+            marker.type = 2
+            marker.action = 0
+            marker.pose.position.x = point[0]
+            marker.pose.position.y = point[1]
+            marker.pose.orientation.w = 1.0
+            marker.scale.x = 1.0
+            marker.scale.y = 1.0
+            marker.scale.z = 1.0
+            marker.color.r = 1.0
+            marker.color.g = 0.0
+            marker.color.b = 0.0
+            marker.color.a = 1.0
+            publisher.publish(marker)
+        
+        visualize_marker(start_u, start_v, map, self.start_marker_pub)
+        visualize_marker(end_u, end_v, map, self.end_marker_pub)
+        
+        def create_visited_node_gridcells(u, v, map):
+            point = self.grid_to_world(u, v, map)
+            grid_cells = GridCells()
+            grid_cells.header.frame_id = "map"
+            grid_cells.cell_width = map.info.resolution
+            grid_cells.cell_height = map.info.resolution
+            grid_cells.cells.append(Point(point[0], point[1], 0))
+            return grid_cells
+
+
+
 
 
         def euclidean_distance(a, b):
@@ -105,26 +133,11 @@ class PathPlan:
             return list(path)
 
         def get_neighbors(u, v):
-            #log the dimensions of the map array
-            # rospy.loginfo("Map array dimensions: {}".format(self.map_array.shape))
-            # #log dimensions of map.info width and height
-            # rospy.loginfo("Map info width: {}".format(map.info.width))
-            # rospy.loginfo("Map info height: {}".format(map.info.height))
-            # #log the value of u and v
-            # rospy.loginfo("U: {}".format(u))
-            # rospy.loginfo("V: {}".format(v))
-            # #log the value of map_array[v, u]
-            # rospy.loginfo("Map array occupancy: {}".format(self.map_array[0, 0]))
-            # # log the min and max indexes of the map array
-            # rospy.loginfo("Map array min index: {}".format(self.map_array.min()))
-            # rospy.loginfo("Map array max index: {}".format(self.map_array.max()))
             neighbors = []
             for du, dv in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
                 if 0 <= u + du < map.info.width and 0 <= v + dv < map.info.height:
-                    # rospy.loginfo("Map array occupancy: {}".format(self.map_array[v + dv, u + du]))
                     if self.map_array[v + dv, u + du] < 50:
                         neighbors.append((u + du, v + dv))
-            # rospy.loginfo("Neighbors: {}".format(neighbors))
             return neighbors
 
         frontier = []
@@ -144,11 +157,14 @@ class PathPlan:
                     priority = new_cost + euclidean_distance(neighbor, (end_u, end_v))
                     heappush(frontier, (priority, neighbor))
                     came_from[neighbor] = current
+                    visited_node_marker = create_visited_node_gridcells(neighbor[0], neighbor[1], map)
+                    self.visited_nodes_pub.publish(visited_node_marker)
+
         else:
-            rospy.loginfo("No path found!")
+            # rospy.loginfo("No path found!")
             return
 
-        rospy.loginfo("Path found!")
+        # rospy.loginfo("Path found!")
         self.trajectory.clear()
         for u, v in path:
             x, y = self.grid_to_world(u, v, map)
